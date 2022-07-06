bea896dd96b96afe0fe23fec5f018bb194620a33
Aswin Kumar <aswinkumar.eniyan@mconnectz.com>
2022-07-06 22:57:28 +0530

    intial commit

--- app.js
+++ app.js
@@ -4,14 +4,10 @@ const port = process.env.PORT || 3000;
 const app = express();
 const routes = require('./routes/router');
 const cors = require('cors');
-const db=require('./db/connect');
-// const bodyParser = require('body-parser');
-// app.use(bodyParser.json());
 
 app.use(cors());
 app.options('*', cors()); 
 app.use(express.json());
-db.connectToServer();
 
 app.listen(port, () =>console.log(`App running on ${port}`));
 
